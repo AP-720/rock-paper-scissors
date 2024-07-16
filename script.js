6353cd346bd1ad1f3e9452533f9aa8b2bf4b1d8d
Andy Price <andyprice720@gmail.com>
2024-07-16 17:09:32 +0100

    Create Human Choice Logic
    Created function to get the human choice. To capitalize it and then return it.
    If no answer or not rock, paper, scissors return an error message.

--- script.js
+++ script.js
@@ -1,31 +1,3 @@
-// Create variable to hold computer choice 
-// function to create computer choice 
-// Randomly select either rock, paper or scissors 
-// Assign the selection to computer choice 
-// 
-
-
-// let computerResponse = getRandomInt(1, 3);
-
-// function getRandomInt(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1) + min);
-// }
-
-// function getComputersChoice(num) {
-
-//     if (num === 1) {
-//         return "Rock";
-//     } else if (num === 2) {
-//         return "Paper";
-//     } else {
-//         return "Scissors";
-//     }
-// }
-
-// let computerChoice = getComputersChoice(computerResponse);
-
-// console.log(computerChoice)
-
 
 
 function getComputersChoice(num) {
@@ -49,4 +21,30 @@ function getComputersChoice(num) {
 
 let computerChoice = getComputersChoice();
 
-console.log(computerChoice)
+
+function getHumanChoice(pick) {
+
+    pick = capitalize(prompt("Enter your choice", "Rock, Paper or Scissors"));
+
+        function capitalize(word) {
+        const lower = word.toLowerCase();
+        return word.charAt(0).toUpperCase() + lower.slice(1);
+    }
+
+    switch (pick) {
+        case "Rock":
+        case "Paper":
+        case "Scissors":
+            return pick;
+            break;
+
+        case "":
+            return ( "Enter your pick. Please enter either Rock, Paper or Scissors")
+        default:
+            return ( "Looks like you made a mistake. Please enter either Rock, Paper or Scissors" );
+    }
+}
+
+let humanChoice = getHumanChoice();
+
+console.log(humanChoice);
